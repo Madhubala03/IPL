ab5246c32f6cb922764fe30b62b765f049d6f753
Madhubala <95579953+Madhubala03@users.noreply.github.com>
2022-03-24 20:04:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import dash_auth
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-USERNAME_PASSWORD_PAIRS=[['guvi','guvi']]
+#USERNAME_PASSWORD_PAIRS=[['guvi','guvi']]
 app=dash.Dash(__name__)
-auth= dash_auth.BasicAuth(app,USERNAME_PASSWORD_PAIRS)
+#auth= dash_auth.BasicAuth(app,USERNAME_PASSWORD_PAIRS)
 server=app.server
 df = pd.read_csv('https://raw.githubusercontent.com/srinathkr07/IPL-Data-Analysis/master/matches.csv')
 df=df.drop(columns='id')
